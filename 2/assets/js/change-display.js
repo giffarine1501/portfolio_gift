41d5e21d1e9d000503bb6e04f6f777466bddef7c
giffarine1501 <giffarine1501@gmail.com>
2023-04-14 13:20:31 +0700

    first commit

---
+++ 2/assets/js/change-display.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    $('#normally').click(function bg_null() {
+        $("body").css("color", "");
+        $("body").css("background-color", "");
+        $(".bg-main").css("background-color", "");
+        $(".bg-blue").css("background-color", "");
+        $(".bg-short").css("background-color", "");
+        $("a").css("color", "");
+        $(".dropdown-item").css("color", "");
+        $(".dropdown-item").css("background-color", "");
+        $(".text_news").css("color", "");
+        $(".date-view").css("color", "");
+        $("h1").css("color", "");
+        $("h5").css("color", "");
+        $(".text_all-btn").css("color", "");
+        $(".btn-all").css("background-color", "");
+        $(".color-topic2").css("color", "");
+        $(".nav-tabs .nav-link").css("background-color", "");
+        $(".card").css("background-color", "");
+        $(".card").css("background-color", "");
+        $(".button-set3").css("background-color", "");
+        $(".button-set3").css("color", "");
+        $(".txtcolor-white").css("color", "");
+        $(".line").css("border-top", "");
+    });
+});
+
+$(document).ready(function () {
+    $('#white').click(function bg_bk() {
+        $("body").css("color", "#FFF");
+        $("body").css("background-color", "#000");
+        $(".bg-main").css("background-color", "#000");
+        $(".bg-blue").css("background-color", "#000");
+        $(".bg-short").css("background-color", "#000");
+        $("a").css("color", "#fff");
+        $(".dropdown-item").css("color", "#fff");
+        $(".dropdown-item").css("background-color", "#000");
+        $(".text_news").css("color", "#fff");
+        $(".date-view").css("color", "#fff");
+        $("h1").css("color", "#fff");
+        $("h5").css("color", "#fff");
+        $(".text_all-btn").css("color", "#000");
+        $(".btn-all").css("background-color", "#fff");
+        $(".color-topic2").css("color", "#fff");
+        $(".nav-tabs .nav-link").css("background-color", "#000");
+        $(".card").css("background-color", "#000");
+        $(".card").css("background-color", "#000");
+        $(".button-set3").css("background-color", "#B0AEAE");
+        $(".button-set3").css("color", "#000");
+        $(".txtcolor-white").css("color", "#fff");
+        $(".line").css("border-top", "1px #fff solid");
+    });
+});
+
+$(document).ready(function () {
+    $('#yellow').click(function bg_yell() {
+        $("body").css("color", "#ffff00");
+        $("body").css("background-color", "#000");
+        $(".bg-main").css("background-color", "#000");
+        $(".bg-blue").css("background-color", "#000");
+        $(".bg-short").css("background-color", "#000");
+        $("a").css("color", "#ffff00");
+        $(".dropdown-item").css("color", "#ffff00");
+        $(".dropdown-item").css("background-color", "#000");
+        $(".text_news").css("color", "#ffff00");
+        $(".date-view").css("color", "#ffff00");
+        $("h1").css("color", "#ffff00");
+        $("h5").css("color", "#ffff00");
+        $(".text_all-btn").css("color", "#000");
+        $(".btn-all").css("background-color", "#ffff00");
+        $(".color-topic2").css("color", "#ffff00");
+        $(".nav-tabs .nav-link").css("background-color", "#000");
+        $(".card").css("background-color", "#000");
+        $(".card").css("background-color", "#000");
+        $(".button-set3").css("background-color", "#ffff00");
+        $(".button-set3").css("color", "#000");
+        $(".txtcolor-white").css("color", "#ffff00");
+        $(".line").css("border-top", "1px #ffff00 solid");
+    });
+});
